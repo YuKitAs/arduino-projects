1c4eb66845cd7a47489a72b0a40a852cea5eff64
YuKitAs <moncher.merci@gmail.com>
2019-09-12 00:20:37 +0200

    Update script

--- dust-detector-with-lcd-display/main.py
+++ dust-detector-with-lcd-display/main.py
@@ -1,8 +1,7 @@
-# Write your code here :-)
 import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-s = [0, 1]
+
 while True:
     read_serial = float(ser.readline().strip())
-    print("{0:.3f}".format(read_serial))
+    print(read_serial)
