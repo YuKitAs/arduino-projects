2ea7575c8d8cd803685b47adc90faa04dceda64d
YuKitAs <moncher.merci@gmail.com>
2019-09-14 00:52:12 +0200

    Send data points to influxdb

--- dust-detector-with-lcd-display/main.py
+++ dust-detector-with-lcd-display/main.py
@@ -1,7 +1,24 @@
+import time
+
 import serial
+from influxdb import InfluxDBClient
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+client = InfluxDBClient(host='localhost', port=8086)
+client.switch_database('dust')
 
 while True:
-    read_serial = float(ser.readline().strip())
-    print(read_serial)
+    dust_value = float(ser.readline().strip())
+    json_body = [{
+        "measurement": "dust_density",
+        "tags": {
+            "location": "home"
+        },
+        "fields": {
+            "value": dust_value
+        }
+    }]
+
+    client.write_points(json_body)
+
+    time.sleep(30)
