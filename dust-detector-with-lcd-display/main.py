5de4342be3cd8b9c394a42fefb99e273fca608e8
YuKitAs <moncher.merci@gmail.com>
2019-09-15 00:14:08 +0200

    Add logging

--- dust-detector-with-lcd-display/main.py
+++ dust-detector-with-lcd-display/main.py
@@ -1,24 +1,34 @@
+import logging
 import time
 
 import serial
 from influxdb import InfluxDBClient
+
+logging.basicConfig(filename='dust.log', format='%(asctime)s - %(levelname)s - %(message)s')
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 client = InfluxDBClient('localhost', 8086)
 client.switch_database('dust')
 
 while True:
-    dust_value = float(ser.readline().strip())
-    json_body = [{
-        "measurement": "dust_density",
-        "tags": {
-            "location": "home"
-        },
-        "fields": {
-            "value": dust_value
-        }
-    }]
+    try:
+        dust_value = float(ser.readline().strip())
 
-    client.write_points(json_body)
+        if dust_value >= 0:
+            json_body = [{
+                "measurement": "dust_density",
+                "tags": {
+                    "location": "home"
+                },
+                "fields": {
+                    "value": dust_value
+                }
+            }]
 
-    time.sleep(30)
+            client.write_points(json_body)
+
+            time.sleep(30)
+        else:
+            logging.warning('Ignored negative value: {}'.format(dust_value))
+    except Exception as ex:
+        logging.error(ex)
