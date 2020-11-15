ac5eaaf5f74b9a64826aef4e5c8876d036ee44f3
YuKitAs <moncher.merci@gmail.com>
2020-11-15 14:57:54 +0100

    Add comments

--- soil-moisture-detector-with-lcd-display/main.ino
+++ soil-moisture-detector-with-lcd-display/main.ino
@@ -5,13 +5,13 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int sensorPin = 0;
 
-const int AIR_VAL = 617;
-const int WATER_VAL = 270;
+const int AIR_VAL = 617; // change to the value measured in the air
+const int WATER_VAL = 270; // change to the value measured in the water
 
 void setupLcd() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Humidity:");
 }
 
@@ -21,17 +21,17 @@ void setup() {
 }
 
 void loop() {
-  int input = analogRead(sensorPin);
+  int input = analogRead(sensorPin); // read soil moisture value
   Serial.println(input);
-  int percentage = map(input, AIR_VAL, WATER_VAL, 0, 100);
+  int percentage = map(input, AIR_VAL, WATER_VAL, 0, 100); // map to range [0, 100]
   Serial.println(percentage);
 
   char output[5];
   sprintf(output, "%d%%", percentage);
-  lcd.setCursor(10,0);
+  lcd.setCursor(10, 0);
   lcd.print(output);
 
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   if (percentage >= 0 && percentage < 33) {
     lcd.print("      Dry       ");
   } else if (percentage >= 33 && percentage < 66) {
