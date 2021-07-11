1d91f5316eb38438c95211d0c366480e30241689
YuKitAs <moncher.merci@gmail.com>
2021-07-11 19:31:12 +0200

    Add a new project

---
+++ temperature-and-humidity-detector-with-display/README.md
@@ -0,0 +1,15 @@
+# Temperature and Humidity Detector with Display
+
+## Components
+
+### Hardware
+
+* Arduino UNO
+* DHT11 Temperature & Humidity Sensor (4 pins)
+* 10k Ω Resistor
+* 16x2 LCD Display with I2C Interface
+
+### Software
+
+* IDE: Arduino IDE
+* Arduino Library: [DHT Sensor Library](https://github.com/adafruit/DHT-sensor-library), [Adafruit Unified Sensor Lib](https://github.com/adafruit/Adafruit_Sensor), [LiquidCrystal_I2C](https://github.com/johnrickman/LiquidCrystal_I2C)
---
+++ temperature-and-humidity-detector-with-display/main.ino
@@ -0,0 +1,44 @@
+#include <DHT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define DHTPIN 2 // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT11 test!"));
+
+  dht.begin();
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  delay(2000);
+
+  // Read relative humidity
+  int h = dht.readHumidity();
+  // Read temperature as Celsius
+  int t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("Humid: ");
+  lcd.print(h);
+  lcd.print("%");
+  lcd.setCursor(3,1);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print("\xDF""C");
+}
