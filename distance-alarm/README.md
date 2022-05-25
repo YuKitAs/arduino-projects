11c1beec5de5e2b25ae939b426ef30cc251cd041
YuKitAs <moncher.merci@gmail.com>
2022-05-25 22:40:45 +0200

    Update code, add connection diagram

---
+++ distance-alarm/README.md
@@ -0,0 +1,18 @@
+# Distance Alarm
+
+A device that generates a warning sound when an object moves within a certain range.
+
+## Components
+
+### Hardware
+
+* Arduino UNO
+* Ultrasonic Sensor HC-SR04
+* Passive Piezo-Buzzer KY-006
+
+### Software
+
+* IDE: Arduino IDE
+
+## Connection
+![](https://raw.githubusercontent.com/YuKitAs/arduino-projects/master/distance-alarm/connection.png)
--- distance-alarm/main.ino
+++ distance-alarm/main.ino
@@ -1,5 +1,6 @@
-const int trigPin = 6; // D6 
 const int echoPin = 5; // D5
+const int trigPin = 6; // D6
+const int buzzPin = 7; // D7
 
 long duration;
 int distance;
@@ -7,25 +8,27 @@ int distance;
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
   // clear trig pin
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
   // trigger and wait for 10 µs
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   duration = pulseIn(echoPin, HIGH);
+  // distance in cm
   distance = (duration * 0.034) / 2;
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  if (distance < 5) {
+    // generates a square wave of frequency 1000Hz, duration 200ms
+    tone(buzzPin, 1000, 200);
+  }
 
   delay(1000);
 }
